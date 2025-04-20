a81befb6566fce3a1f6aa0a5122080cfdc9291cf
poojanaidu <poojanaidu772@gmail.com>
2025-04-20 14:52:12 +0530

    mongourl

--- config/config.js
+++ config/config.js
@@ -2,9 +2,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     // App Settings
-    MONGO_URI: process.env.MONGO_URI || (isProduction
-        ? '' // force Render to use env var only
-        : 'mongodb://localhost:27017/oneonnn'),
+    MONGO_URI: process.env.MONGO_URI
+    //  || (isProduction
+    //     ? '' // force Render to use env var only
+    //     : 'mongodb://localhost:27017/oneonnn'),
+    ,
         TOKEN_SECRET: 'mongoose_project',
     // Other settings...
 };
